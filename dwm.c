680f96e4886b7badf752ae4a1a353939651de2d3
x41v3r <x41v3ryang@gmail.com>
2024-11-25 22:07:57 +0800

    remove the version info on the bar

--- dwm.c
+++ dwm.c
@@ -2218,8 +2218,8 @@ updatesizehints(Client *c)
 void
 updatestatus(void)
 {
-	if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext)))
-		strcpy(stext, "dwm-"VERSION);
+/*	if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext)))
+		strcpy(stext, "dwm-"VERSION);*/
 	drawbar(selmon);
 	updatesystray();
 }
